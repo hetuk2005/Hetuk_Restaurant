f5fe890bbe05132f1df43e91276de44ac1278f92
hetuk2005 <patelhetuk2005@gmail.com>
2026-01-23 20:50:44 +0530

    Scroll Indicator

--- JS/index.js
+++ JS/index.js
@@ -125,4 +125,14 @@ document.addEventListener("click", (e) => {
   }
 });
 
+// Scroll Indicator
+const progressBar = document.querySelector(".scroll_indicator");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  progressBar.style.width = scrollPercent + "%";
+});
+
 apiCall();
