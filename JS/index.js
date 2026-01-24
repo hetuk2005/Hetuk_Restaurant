f7499a5ebb0eb202b37ddd5fbd4d7cf1b5d25f07
hetuk2005 <patelhetuk2005@gmail.com>
2026-01-24 12:42:26 +0530

    Code

--- JS/index.js
+++ JS/index.js
@@ -135,4 +135,30 @@ window.addEventListener("scroll", () => {
   progressBar.style.width = scrollPercent + "%";
 });
 
+// Internet Status
+const statusBox = document.querySelector("#internet");
+
+async function checkInternet() {
+  try {
+    await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      cache: "no-store",
+    });
+    statusBox.innerHTML = "You Are Online ✅";
+    statusBox.classList.remove("Offline");
+    statusBox.classList.add("Online");
+  } catch {
+    statusBox.innerHTML = "You Are Offline ❌";
+    statusBox.classList.remove("Online");
+    statusBox.classList.add("Offline");
+  }
+}
+
+checkInternet();
+
+setInterval(checkInternet, 1000);
+
+// Detect changes in real time
+window.addEventListener("Online", updateStatus);
+window.addEventListener("Offline", updateStatus);
+
 apiCall();
